cd063255ebeabab07fa0d7e3809eabbd4e1f554f
Duvan Villadiego <duvan.villadiego.oficial@gmail.com>
2023-02-07 00:56:27 -0500

    [FIXED] port updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT=process.env.PORT || 3000 ;
+const PORT=process.env.PORT || 3200 ;
 
 const img = require("./src/routes/image/image.js");
 
